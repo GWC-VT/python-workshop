6c16f771e45c6b70cbe4cda8252ed2a6f4700b17
Maggie Lum <maggielizlum@gmail.com>
2024-01-02 18:04:04 -0500

    added another example

---
+++ triangle.py
@@ -0,0 +1,11 @@
+# this code prints a little triangle of "o"s!
+
+for x in range(5):
+    print("o" * x)
+
+
+# suggestions
+
+# run it in a few different sizes by changing 5 to a different number
+# make a variable called size, set it so something, and feed that into range() instead of 5
+# change it to take user input using size = input()
