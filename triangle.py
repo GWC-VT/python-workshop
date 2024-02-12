1e378a3eec52b0050b879613cfa80a2f5f58f082
GWC-VT <153964685+GWC-VT@users.noreply.github.com>
2024-02-12 11:51:55 -0500

    Update triangle.py

--- triangle.py
+++ triangle.py
@@ -1,7 +1,7 @@
 # this code prints a little triangle of "o"s!
 
-for x in range(5):
-    print("o" * x)
+for x in range(50):  # this means x will first be 0, then 1, then 2, and so on
+    print("o" * x)  # prints an 'o' a specific number of times
 
 
 # suggestions
@@ -9,3 +9,4 @@ for x in range(5):
 # run it in a few different sizes by changing 5 to a different number
 # make a variable called size, set it so something, and feed that into range() instead of 5
 # change it to take user input using size = input()
+# write some code on your own that prints characters in a shape other than a triangle
