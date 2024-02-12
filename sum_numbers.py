c30d1bd13be74c5e50d597ce6c3406fa84f701f8
Maggie Lum <maggielizlum@gmail.com>
2024-02-12 11:50:28 -0500

    made some edits to beginner code

--- sum_numbers.py
+++ sum_numbers.py
@@ -1,18 +1,16 @@
-# prints the sum of number up to num
-def sum_numbers(num):
-    x = 1
-    total = 0
-    while x <= num:
-        total = total + x
-        x = x + 1
-    print(total)
+# prints the sum of numbers from 1 to num
 
+my_num = 10  # change this to whatever number you want
+curr_num = 1  # starts at 1, adds 1 to the total, then becomes 2, adds 2 to the total, and so on
+total = 0  # keeps track of the sum
 
-number = input("Please input a number: ")
-sum_numbers(int(number))
+while curr_num <= my_num:
+    total = total + curr_num
+    curr_num = curr_num + 1
+print(total)
 
 # suggestions
 
-# test it and see if it's producing correct results
+# test it and see if it's producing correct results--try manually printing out the sum to see if the answers match!
 # try to modify it to take a user input for num
 # try writing a method to calculate factorials, which are a similar process
